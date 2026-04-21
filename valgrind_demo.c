17c3ff65b9b51b2d55b02f7d84df9d7629a03051
Jorge Romero Garcia <romeroga@unix2.csc.calpoly.edu>
2026-04-21 10:18:50 -0700

    Updated tasks

--- valgrind_demo.c
+++ valgrind_demo.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void my_function(char *);
+void my_function(char **);
 
 int main(int argc, char *argv[]) {
-    char *ptr;
-    ptr = malloc(10);
+    	char *ptr;
+    	ptr = malloc(10);
 
-    if(ptr != NULL) printf("FIRST TEST: ptr is not null\n");
-    else printf("FIRST TEST: ptr is null\n");
+    	if(ptr != NULL) printf("FIRST TEST: ptr is not null\n");
+	else printf("FIRST TEST: ptr is null\n");
 
-    my_function(ptr);
+	my_function(&ptr);
 
-    if(ptr != NULL) printf("SECOND TEST: ptr is not null\n");
-    else printf("SECOND TEST: ptr is null\n");
+	if(ptr != NULL) printf("SECOND TEST: ptr is not null\n");
+	else printf("SECOND TEST: ptr is null\n");
 }
 
-void my_function(char *a) {
-    a = NULL;
+void my_function(char **a) {
+	free(*a);
+	*a = NULL;
 }
